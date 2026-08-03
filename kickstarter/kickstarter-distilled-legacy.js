1d9af5b14c3a1df9e10bd09fd14bfc09f5385da1
TeemClover <86265222+TeemClover@users.noreply.github.com>
2026-08-03 15:34:13 +0700

    Bump final founder runtime patch cache

--- kickstarter/kickstarter-distilled-legacy.js
+++ kickstarter/kickstarter-distilled-legacy.js
@@ -1,2 +1,2 @@
 import './kickstarter-distilled-legacy-base.js?v=20260803-legacy-base';
-import './kickstarter-final-patch.js?v=20260803-final-qa-v1';
+import './kickstarter-final-patch.js?v=20260803-final-qa-v2';
